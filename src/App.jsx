3a69c791e81a6b9181c3ddc344ba1cf40203b3fd
G-Tech Official <127587520+gtech-official08@users.noreply.github.com>
2024-08-13 20:27:26 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,8 @@ function App() {
       <Router>
         <div className="w-full min-h-screen bg-neutral-950 flex flex-col text-neutral-300">
           {/* Navbar */}
-
-
-
-          {/* <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/projects" element={<Project />} />
-            <Route exact path="/articles" element={<Article />} />
-            <Route exact path="/me" element={<About />} />
-
-            <Route exact path="/article-detail" element={<ArticleDetail />} />
-
-          </Routes> */}
+    
+          {/* Routes */}
 
           {/* Footer */}
         </div>
