07163be388be312cab1ffd663e0b813e1aefe485
G-Tech Official <127587520+gtech-official08@users.noreply.github.com>
2024-08-13 20:25:55 +0545

    setup file

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="w-full min-h-screen bg-neutral-950 flex flex-col text-neutral-300">
+          {/* Navbar */}
+
+
+
+          {/* <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/projects" element={<Project />} />
+            <Route exact path="/articles" element={<Article />} />
+            <Route exact path="/me" element={<About />} />
+
+            <Route exact path="/article-detail" element={<ArticleDetail />} />
+
+          </Routes> */}
+
+          {/* Footer */}
+        </div>
+      </Router>
+    </>
+  )
+}
+
+export default App
